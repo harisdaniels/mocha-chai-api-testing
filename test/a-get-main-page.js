e50efbe13d6ccdad710e405531e8715f52a71635
Haris Kaisar Daniels <haris.kaisar@mekari.com>
2024-09-24 07:46:32 +0700

    fix indentation

--- test/a-get-main-page.js
+++ test/a-get-main-page.js
@@ -10,30 +10,30 @@ chai.use(chaiHttp);
 const url = process.env.URL;
 describe('As a customer, I want to see the Main Page', () => {
 
-    it("It should SUCCESS to load Welcome Page", (done) => {
-        chai.request(url)
-            .get("/")
-            .then((response) => {
-                assert.equal(response.status, 200);
-                assert.equal(response.body.message, "Welcome to the Simple Books API.");
-                done();
-            })
-            .catch((error) => {
-                done(error);
-            });
-    });
+  it("It should SUCCESS to load Welcome Page", (done) => {
+    chai.request(url)
+      .get("/")
+      .then((response) => {
+          assert.equal(response.status, 200);
+          assert.equal(response.body.message, "Welcome to the Simple Books API.");
+          done();
+      })
+      .catch((error) => {
+          done(error);
+      });
+  });
 
-    it("It should SUCCESS to load Status Page", (done) => {
-        chai.request(url)
-            .get("/status")
-            .then((response) => {
-                assert.equal(response.status, 200);
-                assert.equal(response.body.status, "OK");
-                done();
-            })
-            .catch((error) => {
-                done(error);
-            });
-    });
+  it("It should SUCCESS to load Status Page", (done) => {
+    chai.request(url)
+      .get("/status")
+      .then((response) => {
+          assert.equal(response.status, 200);
+          assert.equal(response.body.status, "OK");
+          done();
+      })
+      .catch((error) => {
+          done(error);
+      });
+  });
 
 });
